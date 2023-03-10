4d6395ed2e26568499f25af8317a6cefa79a107d
Maxxbay <maxxbay@gmail.com>
2023-03-10 14:04:06 +0100

    new one

---
+++ react-authentication-backtend/src/routes/index.js
@@ -0,0 +1,5 @@
+import { testRoute } from "./testRoute.js";
+import { signUpRoute } from "./signUpRoute.js";
+import { logInRoute } from "./loginRoute.js";
+
+export const routes = [testRoute, signUpRoute, logInRoute];
